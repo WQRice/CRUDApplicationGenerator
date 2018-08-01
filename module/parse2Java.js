8765220ebcad48cd058b01a0de839d12248e83d4
WQRice <wq2@rice.edu>
2018-08-01 14:39:27 -0500

    Update 2018-08-01T19:38:59.651Z

--- module/parse2Java.js
+++ module/parse2Java.js
@@ -9,17 +9,17 @@ module.exports = function (datas,outputPath,modulePath) {
         //produce controller
         var template = fs.readFileSync(modulePath+'/backendTemplate/controller.ejs', 'utf-8');
         var file = ejs.render(template, datas[i]);
-        fs.outputFileSync(outputPath+"/src/main/java/OITWeb/" + datas[i].PACKAGENAME.replace(/./g,'/') + "/controller/" + datas[i].CLASSNAME + 'Controller.java', file, 'utf8');
+        fs.outputFileSync(outputPath+"/src/main/java/OITWeb/" + datas[i].PACKAGENAME.replace(/\./g,'/') + "/controller/" + datas[i].CLASSNAME + 'Controller.java', file, 'utf8');
 
         //produce entity class
         var template = fs.readFileSync(modulePath+'/backendTemplate/entity.ejs', 'utf-8');
         var file = ejs.render(template, datas[i]);
-        fs.outputFileSync(outputPath+"/src/main/java/OITWeb/" + datas[i].PACKAGENAME.replace(/./g,'/') + "/model/" + datas[i].CLASSNAME + '.java', file, 'utf8');
+        fs.outputFileSync(outputPath+"/src/main/java/OITWeb/" + datas[i].PACKAGENAME.replace(/\./g,'/') + "/model/" + datas[i].CLASSNAME + '.java', file, 'utf8');
 
         //produce repository class
         var template = fs.readFileSync(modulePath+'/backendTemplate/repository.ejs', 'utf-8');
         var file = ejs.render(template, datas[i]);
-        fs.outputFileSync(outputPath+"/src/main/java/OITWeb/" + datas[i].PACKAGENAME.replace(/./g,'/') + "/repository/" + datas[i].CLASSNAME + 'Repository.java', file, 'utf8');
+        fs.outputFileSync(outputPath+"/src/main/java/OITWeb/" + datas[i].PACKAGENAME.replace(/\./g,'/') + "/repository/" + datas[i].CLASSNAME + 'Repository.java', file, 'utf8');
     }
 
 
@@ -27,7 +27,7 @@ module.exports = function (datas,outputPath,modulePath) {
         //produce exception class
         var template = fs.readFileSync(modulePath+'/backendTemplate/exception.ejs', 'utf-8');
         var file = ejs.render(template, datas[0]);
-        fs.outputFileSync(outputPath+"/src/main/java/OITWeb/" + datas[0].PACKAGENAME.replace(/./g,'/') + "/exception/" + 'ResourceNotFoundException.java', file, 'utf8');
+        fs.outputFileSync(outputPath+"/src/main/java/OITWeb/" + datas[0].PACKAGENAME.replace(/\./g,'/') + "/exception/" + 'ResourceNotFoundException.java', file, 'utf8');
 
         //produce entry class
         var template = fs.readFileSync(modulePath+'/backendTemplate/application.ejs', 'utf-8');
@@ -36,6 +36,6 @@ module.exports = function (datas,outputPath,modulePath) {
 
         var template = fs.readFileSync(modulePath+'/backendTemplate/repositoryAdapter.ejs', 'utf-8');
         var file = ejs.render(template, datas[0]);
-        fs.outputFileSync(outputPath+"/src/main/java/OITWeb/" + datas[0].PACKAGENAME.replace(/./g,'/') + "/repository/" + 'RepositoryAdapter.java', file, 'utf8');
+        fs.outputFileSync(outputPath+"/src/main/java/OITWeb/" + datas[0].PACKAGENAME.replace(/\./g,'/') + "/repository/" + 'RepositoryAdapter.java', file, 'utf8');
     }
 }
