fc60da6d172e46362bf60346cc477789f8b7fc3d
WQRice <wq2@rice.edu>
2018-08-03 14:14:41 -0500

    Update 2018-08-03T19:14:20.448Z

--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,5 @@ var ghPages = require('gulp-gh-pages');
 //Deploy code to github branch
 gulp.task('deploy', function() {
   return gulp.src('./**/*')
-    .pipe(ghPages({'remoteUrl':'https://github.com/WQRice/CRUDApplicationGenerator.git','branch':'Deploy'}));
+    .pipe(ghPages({'remoteUrl':'https://github.com/WQRice/CRUDApplicationGenerator.git','branch':'mileStone1'}));
 });
